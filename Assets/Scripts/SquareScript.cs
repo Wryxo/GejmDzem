93784c1772d1fc8f642c9d938d9fb94abaf47549
Martin Strapko <martin.strapko@microstep-hdo.sk>
2015-05-16 10:32:58 +0200

    Prefaby a scripty pre stvorcek a panacika...

--- Assets/Scripts/SquareScript.cs
+++ Assets/Scripts/SquareScript.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class SquareScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        
+    private SpriteRenderer _spriteRenderer;
+    private Color[] _colors = new Color[] { Color.red, Color.blue, Color.green, Color.cyan, Color.magenta };
+    private int _currentColor = 0;
+    private Transform _transform;
+    private float _speed = 1.0f;
+    public int Id = -1;
+
+
+    // Use this for initialization
+    void Start () {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _transform = GetComponent<Transform>();
+        _spriteRenderer.color = _colors[0];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+    }
+
+    void FixedUpdate()
+    {
+        _transform.position += new Vector3(_speed * Time.deltaTime, 0.0f);
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Click");
+
+        int rand = Random.Range(0, 5);
+
+        if (rand != _currentColor)
+        {
+            _spriteRenderer.color = _colors[_currentColor];
+            _currentColor = rand;
+        }
+    }
 }
